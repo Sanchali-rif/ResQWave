312e0ecb7b74a22ff9954c25f6103e488c254584
Sanchali-rif <sanchalisaha05@gmail.com>
2026-04-19 11:39:56 +0530

    alert screen

--- src/screens/SOSScreen.js
+++ src/screens/SOSScreen.js
@@ -1,9 +1,60 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 export default function SOSScreen() {
     return (
-        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-            <Text>SOS SCREEN</Text>
+        <View style={styles.container}>
+
+            {/* Title */}
+            <Text style={styles.title}>EMERGENCY SOS</Text>
+
+            {/* Big SOS Button */}
+            <TouchableOpacity style={styles.sosButton}>
+                <Text style={styles.sosText}>SOS</Text>
+            </TouchableOpacity>
+
+            {/* Instruction */}
+            <Text style={styles.subText}>
+                Press and hold to send emergency alert
+            </Text>
+
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#0f0f10",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+
+    title: {
+        color: "#ff6b6b",
+        fontSize: 20,
+        fontWeight: "bold",
+        marginBottom: 40,
+    },
+
+    sosButton: {
+        width: 150,
+        height: 150,
+        borderRadius: 75,
+        backgroundColor: "#c1121f",
+        justifyContent: "center",
+        alignItems: "center",
+        elevation: 10,
+    },
+
+    sosText: {
+        color: "white",
+        fontSize: 28,
+        fontWeight: "bold",
+    },
+
+    subText: {
+        color: "#aaa",
+        marginTop: 30,
+        fontSize: 12,
+    },
+});
